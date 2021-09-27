9c23c84349e44d47bef2d107b99dd2e33626b7e0
Djordje Cvetkovic <djole001@gmail.com>
2021-09-27 13:21:07 +0200

    initial

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let acc = document.getElementsByClassName("accordion");
 
 
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
